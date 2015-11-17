500451d4304c644321139cc9d6a49bd41b47d06c
neoyoos <ksooyoun00@naver.com>
2015-11-17 17:43:30 +0900

    #no-issue

--- virtualLego.cpp
+++ virtualLego.cpp
@@ -26,12 +26,12 @@ const int Width = 1024;
 const int Height = 768;
 int score = 0;
 int interSectedCount = 0;
-const int NUM_OF_SPHERE = 8;
+const int NUM_OF_SPHERE = 10;
 // There are four balls
 // initialize the position (coordinate) of each ball (ball0 ~ ball3)
-const float spherePos[NUM_OF_SPHERE][2] = { {-0.7f,-0.0f}  , {1.2f, 2.3f}, {1.2f, 0.0f},{ -0.8f,-0.9f },{1.2f, -2.3f}, {3.2f, 2.3f }, {3.2f, 0.0f}, {3.2f, -2.3f} };
+const float spherePos[NUM_OF_SPHERE][2] = { {-0.7f,-2.3f} , {-0.7f,-0.0f} , {-0.7f,2.3f} , {-0.8f,-0.9f}, {1.2f, 2.3f}, {1.2f, 0.0f},  {1.2f, -2.3f}, {3.2f, 2.3f }, {3.2f, 0.0f}, {3.2f, -2.3f} };
 // initialize the color of each ball (ball0 ~ ball3)
-const D3DXCOLOR sphereColor[NUM_OF_SPHERE] = { d3d::MAGENTA,  d3d::YELLOW, d3d::YELLOW, d3d::WHITE, d3d::YELLOW, d3d::BLUE,d3d::BLUE,d3d::BLUE };
+const D3DXCOLOR sphereColor[NUM_OF_SPHERE] = { d3d::MAGENTA, d3d::MAGENTA, d3d::MAGENTA, d3d::WHITE, d3d::YELLOW, d3d::YELLOW, d3d::YELLOW, d3d::BLUE,d3d::BLUE,d3d::BLUE };
 
 // -----------------------------------------------------------------------------
 // Transform matrices
@@ -99,8 +99,8 @@ public:
 			return;
 		pDevice->SetTransform(D3DTS_WORLD, &mWorld);
 		pDevice->MultiplyTransform(D3DTS_WORLD, &m_mLocal);
-pDevice->SetMaterial(&m_mtrl);
-m_pSphereMesh->DrawSubset(0);
+		pDevice->SetMaterial(&m_mtrl);
+		m_pSphereMesh->DrawSubset(0);
 	}
 
 	bool hasIntersected(CSphere& ball)
@@ -126,7 +126,7 @@ m_pSphereMesh->DrawSubset(0);
 	void hitBy(CSphere& ball)
 	{
 		if (hasIntersected(ball)) {
-			score += 10;
+			score+=10;
 			D3DXVECTOR3 ballCenter = ball.getCenter();
 
 			float collisionX = normalizeX(this->center_x - ballCenter.x, this->center_z - ballCenter.z);
@@ -148,7 +148,6 @@ m_pSphereMesh->DrawSubset(0);
 			//float v2z = ball.getVelocity_Z() - transVAz + transVBz;
 
 			setCenter(this->center_x + 0.01 * collisionX, this->center_y, this->center_z + 0.01 * collisionZ);
-
 			setPower(1.5*v1x, 1.5*v1z);
 			//ball.setPower(v2x, v2z);
 		}
@@ -199,17 +198,10 @@ m_pSphereMesh->DrawSubset(0);
 		{
 			this->center_x = 4.56f - this->getRadius();
 		}
-<<<<<<< HEAD
 		//if (this->center_x - this->getRadius() < -4.56f)
 		//{
 		//	this->center_x = -4.56f + this->getRadius();
 		//}
-=======
-		if ((this->center_x + this->getRadius() < -4.56f) && (this->center_z > 0.2f) && (this->center_z < -0.2f))
-		{
-			this->center_x = -4.56f + this->getRadius();
-		}
->>>>>>> origin/master
 	}
 
 	double getVelocity_X() { return this->m_velocity_x; }
@@ -333,6 +325,12 @@ public:
 				return true;
 			}
 		}
+		else if (this->m_x > -3.1f && this->m_x < -2.9f){ 
+			if (1.5 - ball.getRadius() < pos.x) {
+				return true;
+			}
+			
+		}
 		else {
 			if (0 < this->m_z) {
 				if (3 - ball.getRadius() < pos.z) {
@@ -397,6 +395,7 @@ public:
 
 	void hitRodBy(CSphere& ball){
 		if (hasRodIntersected(ball)){
+			
 			float rodRotation = this->getRotation();
 			if (rodRotation < 0) rodRotation *= (-1);
 
@@ -535,7 +534,7 @@ private:
 // Global variables
 // -----------------------------------------------------------------------------
 CWall	g_legoPlane;
-CWall	g_legowall[5];
+CWall	g_legowall[4];
 CWall   g_rod[2];
 CSphere	g_sphere[NUM_OF_SPHERE];
 //CSphere	g_target_blueball;
@@ -574,16 +573,9 @@ bool Setup()
 	g_legowall[1].setPosition(0.0f, 0.12f, -3.06f);
 	if (false == g_legowall[2].create(Device, -1, -1, 0.12f, 0.3f, 6.24f, d3d::DARKRED)) return false;
 	g_legowall[2].setPosition(4.56f, 0.12f, 0.0f);
-<<<<<<< HEAD
 	//if (false == g_legowall[3].create(Device, -1, -1, 0.12f, 0.3f, 6.24f, d3d::DARKRED)) return false;
 	//g_legowall[3].setPosition(-4.56f, 0.12f, 0.0f);
 
-=======
-	if (false == g_legowall[3].create(Device, -1, -1, 0.12f, 0.3f, 2.8f, d3d::DARKRED)) return false;
-	g_legowall[3].setPosition(-4.56f, 0.12f, -1.8f);
-	if (false == g_legowall[4].create(Device, -1, -1, 0.12f, 0.3f, 2.8f, d3d::DARKRED)) return false;
-	g_legowall[4].setPosition(-4.56f, 0.12f, 1.8f);
->>>>>>> origin/master
 	if (g_rod[0].create(Device, -1, -1, 0.12f, 0.3f, 3.0f, d3d::DARKRED) == false) {
 		return false;
 	}
@@ -640,7 +632,7 @@ bool Setup()
 	Device->SetRenderState(D3DRS_SHADEMODE, D3DSHADE_GOURAUD);
 
 	g_light.setLight(Device, g_mWorld);
-	D3DXCreateFont(Device, 60, 0, FW_BOLD, 0, FALSE, DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH | FF_DONTCARE, TEXT("Arial"), &g_font);
+	D3DXCreateFont(Device, 25, 0, FW_BOLD, 0, FALSE, DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH | FF_DONTCARE, TEXT("Arial"), &g_font);
 	D3DXCreateFont(Device, 20, 0, FW_BOLD, 0, FALSE, DEFAULT_CHARSET, OUT_DEFAULT_PRECIS, DEFAULT_QUALITY, DEFAULT_PITCH | FF_DONTCARE, TEXT("Arial"), &g_debug);
 	return true;
 }
@@ -648,11 +640,7 @@ bool Setup()
 void Cleanup(void)
 {
 	g_legoPlane.destroy();
-<<<<<<< HEAD
 	for (int i = 0; i < 3; i++) {
-=======
-	for (int i = 0; i < 5; i++) {
->>>>>>> origin/master
 		g_legowall[i].destroy();
 	}
 	destroyAllLegoBlock();
@@ -676,16 +664,7 @@ bool Display(float timeDelta)
 		// update the position of each ball. during update, check whether each ball hit by walls.
 		for (i = 0; i < NUM_OF_SPHERE; i++) {
 			g_sphere[i].ballUpdate(timeDelta);
-<<<<<<< HEAD
 			if(i < 3) g_legowall[i].hitBy(g_sphere[3]);
-=======
-			if (i < 5) {
-				g_legowall[i].hitBy(g_sphere[3]);
-			}
-			if (i < 2) {
-				//g_rod[i].hitRodBy(g_sphere[3]);
-			}				
->>>>>>> origin/master
 		}
 		// 하얀 공에 중력 적용
 		g_sphere[3].setPower(g_sphere[3].getVelocity_X() + timeDelta * (-9.8), g_sphere[3].getVelocity_Z());
@@ -700,11 +679,7 @@ bool Display(float timeDelta)
 
 		// draw plane, walls, and spheres
 		g_legoPlane.draw(Device, g_mWorld);
-<<<<<<< HEAD
 		for (i = 0; i < 3; i++) {
-=======
-		for (i = 0; i < 5; i++) {
->>>>>>> origin/master
 			g_legowall[i].draw(Device, g_mWorld);
 		}
 		for (i = 0; i < NUM_OF_SPHERE; i++) {
@@ -723,10 +698,16 @@ bool Display(float timeDelta)
 
 		// Create a rectangle to indicate where on the screen it should be drawn
 		RECT scoreRect;
-		scoreRect.left = 50;
+		scoreRect.left = 10;
 		scoreRect.right = 780;
-		scoreRect.top = 50;
-		scoreRect.bottom = scoreRect.top + 80;
+		scoreRect.top = 10;
+		scoreRect.bottom = scoreRect.top + 20;
+		
+		RECT debugRect;
+		debugRect.left = 10;
+		debugRect.right = 780;
+		debugRect.top = 50;
+		debugRect.bottom = debugRect.top + 20;
 
 
 		// Draw some text
@@ -735,6 +716,7 @@ bool Display(float timeDelta)
 		_itoa_s(score, scoreBuffer, 20, 10);
 		_itoa_s(interSectedCount, debugBuffer, 20, 10);
 		g_font->DrawText(NULL, scoreBuffer, -1, &scoreRect, 0, fontColor);
+		g_debug->DrawTextA(NULL, debugBuffer, -1, &debugRect, 0, fontColor);
 		Device->EndScene();
 		Device->Present(0, 0, 0, 0);
 		Device->SetTexture(0, NULL);
